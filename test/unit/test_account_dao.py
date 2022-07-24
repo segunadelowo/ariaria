c55179df722a3fc1a3bd700302bae99d5c3209d4
Segun Adelowo <segun.adelowo@interswitchgroup.com>
2022-07-24 15:15:12 +0100

    add test

--- test/unit/test_account_dao.py
+++ test/unit/test_account_dao.py
@@ -6,7 +6,7 @@ from ariaria.utils.enums import AccountStatus
 
 
 
-def test_repository_can_save_an_account(session):
+def test_raccountdao_can_save_an_account(session):
     acct = Account(1,'testuser','vghvhgvghvgh','test_name1','test_email.com','09090909','989 khjhhkkh kkh yyuyuyuu', AccountStatus.UNKNOWN.value)
 
     repo = AccountDao(session)
@@ -17,3 +17,16 @@ def test_repository_can_save_an_account(session):
         'SELECT id, email, phone FROM "accounts"'
     )
     assert list(rows) == [(1, "test_email.com", '09090909')]
+
+
+def test_accountdao_can_get_account(session):
+   
+    session.execute(
+        "INSERT INTO accounts (id, 'user_name', 'password', 'name', 'email', 'phone', 'shipping_address', 'status')"
+        " VALUES (1,'testuser','vghvhgvghvgh','test_name1','test_email.com','09090909','989 khjhhkkh kkh yyuyuyuu', 6)"
+    )
+
+    repo = AccountDao(session)
+    acct  = repo.get(1)
+
+    assert acct.user_name == 'testuser'
